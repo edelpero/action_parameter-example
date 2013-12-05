e61a85cf4f70935bd1bfd04acd238b0ed3ad6317
Ezequiel Delpero <edelpero@gmail.com>
2013-12-05 14:57:38 -0300

    Configures devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ ActionParameterExample::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.assets.initialize_on_precompile = false
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
